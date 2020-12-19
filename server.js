a26e6d02880963827366d8955b55cf68b49ef0b1
Billy Sheehan <66801979+billysheehan@users.noreply.github.com>
2020-12-19 00:12:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,23 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
+// parse application
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
 app.use(bodyParser.json());
 
-//method override
+// override
  app.use(methodOverride("_method"))
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
+// More handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+// Import routes 
 var routes = require("./controllers/burgersController.js");
 
 app.use("/", routes);
