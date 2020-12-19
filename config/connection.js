99fa9c852d65daaaed00104b69f33d0139614216
Billy Sheehan <66801979+billysheehan@users.noreply.github.com>
2020-12-18 23:57:05 -0600

    Add files via upload

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+//configure connection to mysql database
+var connection
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	});
+};
+// Make connection with above configuration
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
